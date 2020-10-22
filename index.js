4d9df15bda9e37213128885ce84f24e3d07c28b2
liudmylamakarchuk888 <liudmylamakarchuk888@gmail.com>
2020-10-23 01:51:43 +0900

    add socket

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const express = require("express");
-const path = require("path");
-let socketIo = require("socket.io");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const socketProxy = createProxyMiddleware("/socket.io", {
-  target: "http://localhost:8000",
-  logLevel: "debug",
-});
+var app = require("express")();
+var http = require("http").createServer(app);
 
-const PORT = process.env.PORT || 5000;
-const app = express();
-var io = socketIo();
-app.io = io;
+var io = require("socket.io")(http);
+
 let clients = [];
 
 io.on("connection", (socket) => {
+
   socket.on("store-client-info", (data) => {
     const clientInfo = new Object();
-
+    console.log("Asdfasdfsadf");
     clientInfo.uid = data.uid;
     clientInfo.clientId = socket.id;
 
     clients.push(clientInfo);
   });
 
+//   socket.on("new-member", (data) => {
+//     console.log(data);
+
+//     clients.forEach(client => {
+//         io.sockets.sockets[client.clientId].emit('new-member', data);
+//     });
+//   });
+
   socket.on("message", (data) => {
-    clients.forEach((client) => {
-      if (data.recipients.includes(client.uid)) {
-        io.sockets.sockets[client.clientId].emit("message", {
-          senderId: data.senderUserId,
-          chatRoomId: data.chatRoomId,
-          isGroupChat: data.isGroupChat,
-          text: data.text,
-        });
-      }
+
+    clients.forEach(client => {
+        if(data.recipients.includes(client.uid)) {
+            io.sockets.sockets[client.clientId].emit('message', {
+                senderId: data.senderUserId,
+                chatRoomId: data.chatRoomId,
+                isGroupChat: data.isGroupChat,
+                text: data.text
+            });
+        }
     });
   });
 
@@ -48,10 +50,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app
-  .use(express.static(path.join(__dirname, "public")))
-  .use(socketProxy)
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get("/", (req, res) => res.send("hello!"));
+http.listen(8000, () => {
+  console.log("listening on *:8000");
+});
